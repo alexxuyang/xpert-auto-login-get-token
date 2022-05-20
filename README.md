5529eb70487e42ccfbb92397f4db7fbcf26f7f0a
xuyang <yang.xu@nbltrust.com>
2022-05-20 15:03:58 +0800

    add config and REAME

---
+++ README.md
@@ -0,0 +1,14 @@
+运行`npm install`
+
+修改`config.json`中的email和password。
+
+运行`node main.js`
+
+期待的运行结果：
+
+```
+[2022-05-20T07:02:24.412Z] logined
+[2022-05-20T07:02:27.732Z] get code
+pls input the auth code:208223
+[2022-05-20T07:02:40.960Z] token:eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySUQiOjQ4NywiVFRMIjo0MzIwLCJUeXBlIjoiQVVUSF9DT0RFX1ZFUklGSUVEIiwiQ2xpZW50VHlwZSI6Ik1PQklMRSIsImV4cCI6MTgxMDcxMDE2MX0.EoczQOzEbtSraW1IcFGuyWW5zKLC1WTMy7zwqrqZh0Y
+```
--- main.js
+++ main.js
@@ -1,38 +1,37 @@
 require('log-timestamp');
 const waitForUserInput = require('wait-for-user-input')
 const axios = require('axios');
+const config = require('./config.json');
 
 async function main () {
     var result = await axios({
         method: 'post',
-        url: 'https://develop-saas.nbltrust.com/saas-bg/api/v1/session',
+        url: config.login_url,
         headers: { 'content-type': 'application/json' },
-        data: {"email": "test.008@jadewallet.io", "password": "Test1234", "deviceID": "123dsdf", "geetestResponse": {"lotNumber": "", "captchaOutput": "", "passToken": "", "genTime": ""}}
+        data: {"email": config.email, "password": config.password, "deviceID": "123dsdf", "geetestResponse": {"lotNumber": "", "captchaOutput": "", "passToken": "", "genTime": ""}}
       });
 
     let token = result.data.data.token;
 
     // console.log(token);
-    console.log(1);
+    console.log("logined");
 
     result = await axios({
         method: 'get',
-        url: 'https://develop-saas.nbltrust.com/saas-bg/api/v1/emailcode?action=login',
+        url: config.code_url,
         headers: {
             'Accept': 'application/json',
             'Authorization': 'Bearer ' + token
         }
     });
     // console.log(result.data.data);
-    console.log(2);
+    console.log("get code");
 
     const auth_code = await waitForUserInput('pls input the auth code:');
 
-    console.log(3);
-
     result = await axios({
         method: 'put',
-        url: 'https://develop-saas.nbltrust.com/saas-bg/api/v1/account/verify-authcode',
+        url: config.verify_url,
         headers: {
             'content-type': 'application/json',
             'X-EmailCode': auth_code,
@@ -40,7 +39,7 @@ async function main () {
         }
     });
 
-    console.log(result.data.data.token);
+    console.log("token:" + result.data.data.token);
 }
 
 main().catch(console.error).finally(() => process.exit());
